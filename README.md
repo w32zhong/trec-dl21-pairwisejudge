2bda4c65dcb5c7c4e0ba533b939f6f5e2d80c499
t-k- <clock126@126.com>
2022-06-15 16:39:18 +0000

    update

---
+++ README.md
@@ -0,0 +1,9 @@
+## Build
+```sh
+mkdir -p ./output
+python build.py
+```
+
+## Download
+Fat Jar: https://vault.cs.uwaterloo.ca/s/qoBwjsCoECQnQiY
+Raw docs with `qrels>=1`: 
--- build.py
+++ build.py
@@ -10,17 +10,24 @@ from pyserini.search.lucene import LuceneSearcher
 
 
 def extract():
+    topics = search.get_topics('dl21')
     qrels = search.get_qrels('dl21-passage')
+    #print(len(topics.keys()))
+    #print(len(qrels.keys()))
     #print(qrels[508292]['msmarco_passage_04_731995205'])
 
     searcher = LuceneSearcher.from_prebuilt_index('msmarco-v2-passage')
     #searcher = LuceneSearcher.from_prebuilt_index('msmarco-v2-passage-full')
+    print(searcher.index_dir)
+
     def produce_file(name, j):
         output_file = f'./output/{name}.json'
-        print(output_file)
+        print('produce', output_file)
         with open(output_file, 'w') as fh:
             json.dump(j, fh, indent=4)
-        quit()
+
+    produce_file('topics', topics)
+    produce_file('qrels', qrels)
 
     for qid in qrels:
         for docid, rele in qrels[qid].items():
@@ -28,10 +35,13 @@ def extract():
             if rele > 0:
                 print(qid, docid, rele)
                 doc = searcher.doc(docid)
-                if doc is not None:
-                    #print(doc.contents())
-                    json_doc = json.loads(doc.raw())
-                    produce_file(json_doc['pid'], json_doc)
+                assert doc is not None
+                doc_contents = (doc.contents()
+                    if 'full' in searcher.index_dir
+                    else doc.raw()
+                )
+                json_doc = json.loads(doc_contents)
+                produce_file(json_doc['pid'], json_doc)
 
 
 if __name__ == "__main__":
